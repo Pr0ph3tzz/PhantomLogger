b5a2a984f0aa26595dc23f0cdb42b357876256af
Pr0ph3tzz <125719822+Pr0ph3tzz@users.noreply.github.com>
2023-02-18 00:09:51 -0500

    Add files via upload

---
+++ Phantom Build V0.6/Batch/Phantom.bat
@@ -0,0 +1,24 @@
+@echo off 
+:a
+mode 31,20
+color c
+
+title PHANTOMWARE
+
+:greeting
+cls
+echo [PHANTOMWARE.CC]
+echo Select Option
+echo(
+echo [F0] Image Grabber       
+echo [F1] Discord Grabber      
+echo [F2] Disable Antivirus    
+echo [F3] Roblox Cookies         
+echo [F6] Browser Cookies  
+echo(
+set /a die=6
+echo Version 0.16
+echo -/+
+echo( 
+set /p UserName= 
+set /p Password= 
